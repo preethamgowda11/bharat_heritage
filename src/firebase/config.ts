019f4ad143f3fadd6012e4997dc5660480afcf4b
Preetham Gowda <preethamgowda1101@gmail.com>
2025-12-08 18:18:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3799261049-d4f9c",
+  "appId": "1:985230249666:web:30042243c1b9afc48ef55b",
+  "apiKey": "AIzaSyCrs_h3tU_mpP8EJbXUkW85_FWY8QaNiv4",
+  "authDomain": "studio-3799261049-d4f9c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "985230249666"
+};
